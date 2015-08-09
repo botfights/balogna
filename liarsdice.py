d781a544c2f7c22239036752339a69787af970e9
Colin M. Saunders <colinmsaunders@gmail.com>
2015-08-08 19:14:58 -0700

    doesn't work

--- liarsdice.py
+++ liarsdice.py
@@ -1,5 +1,31 @@
 # liarsdice.py -- the liar's dice logic functions, you can import this in your robots
 
+HELP = '''\
+usage:
+
+    To play a game against the computer:
+
+        $ python main.py play p_human p_computer
+
+    To play a 10 game tourney between p_robot and p_computer and p_dummy: 
+
+        $ python main.py tournament 10 p_robot p_human p_dummy
+'''
+
+import sys
+import imp
+import logging
+import random
+import time
+
+# ignore SIG_PIPE
+from signal import (signal,
+                    SIGPIPE,
+                    SIG_DFL)
+
+signal(SIGPIPE, SIG_DFL)
+
+
 # the variation of liars dice (5 6-sided dies, ones not wild)
 #
 RULES_DICE          = 5
@@ -234,3 +260,213 @@ def play_game(game_id,players,player_names,catch_exceptions) :
     logging.info('player %s-%s wins' % (winner,player_names[winner]))
     return winner
 
+def play_games(n,seed,playernames,catch_exceptions) :
+    random.seed(seed)
+    logging.debug('SEED\t%s' % seed)
+    players = {}
+    scores = {}
+    names = {}
+    for i in playernames :
+        playername, path, modulename, attr = split_playername(i)
+        logging.info('playername: %s => %s' % (i,split_playername(i)))
+        player_id = chr(ord('A') + len(players))
+        names[player_id] = playername
+        logging.info('making player %s (%s) ...' % (player_id,playername))
+        p = make_player(playername, path, modulename, attr, catch_exceptions)
+        players[player_id] = p
+        scores[player_id] = 0
+    game_num = 0
+    for r in range(n) :
+        game_num += 1
+        logging.debug('playing game %d ...' % (game_num,))
+        winner = liarsdice.play_game(game_num,players,names,catch_exceptions)
+        scores[winner] += 1
+        logging.debug('RESULT\tgame:%d\twinner:%s' % (game_num,winner))
+        k = scores.keys()
+        k.sort(key = lambda x : scores[x],reverse = True)
+        rank = 0
+        for i in k :
+            rank += 1
+            logging.info('SCORE\tgame %d of %d\t#%d.\t%s\t%s\t%d' % (game_num,n,rank,i,names[i],scores[i]))
+        logging.info('SCORE')
+        logging.info('STATUS\t%.2f\t\t%s' % (game_num/float(n),','.join(map(lambda i : '%s:%s' % (names[i],scores[i]),k))))
+    return scores
+
+def get_play(player, my_id, her_id, last):
+    x = None
+    try:
+        x = player.get_play(my_id, her_id, last)
+    except:
+        pass
+    if not x in (1, 2, 3):
+        x = random.choice((1, 2, 3))
+    return x
+
+def play_tournament(t, n, players) :
+    scores = {}
+    for i in range(len(players)):
+        scores[i] = 0
+    for r in range(t) :
+        for i in range(len(players)) :
+            for j in range(len(players)) :
+                if i >= j :
+                    continue
+                x = play_game(n, players[i], players[j])
+                if 0 == x :
+                    scores[i] += 1
+                else :
+                    scores[j] += 1
+                logging.info('SCORE\t%d\t%d\t%d\t%d\t%d\t%d\t%s\t%s' % (r,t,i,j,scores[i],scores[j],players[i].playername,players[j].playername))
+        logging.info('BOTFIGHTS\t%d\t%d\t%s' % (r, t,'\t'.join(map(lambda i : '%s:%s' % (players[i].playername,scores[i]),range(len(players))))))
+    logging.info('BOTFIGHTS\t%d\t%d\t%s' % (t, t,'\t'.join(map(lambda i : '%s:%s' % (players[i].playername,scores[i]),range(len(players))))))
+    return -1
+
+
+class Player():
+    pass
+
+
+def call_player(player, args, default):
+    global g_catch_exceptions
+    result = default
+    start = time.clock()
+    try:
+        result = player.play(*args)
+    except KeyboardInterrupt:
+        raise
+    except:
+        logging.warn('caught exception "%s" calling %s (%s)'
+                     % (sys.exc_info()[1], player.player_id, player.playername))
+        if not g_catch_exceptions:
+            raise
+    elapsed = time.clock() - start
+    player.elapsed += elapsed
+    player.calls += 1
+    return result
+
+
+def make_player(player_id, dirname):
+    global g_catch_exceptions
+    m = None
+    try:
+        name = 'bot'
+        (f, filename, data) = imp.find_module(name, [dirname, ])
+        m = imp.load_module(name, f, filename, data)
+    except:
+        logging.error('caught exception "%s" loading %s' % \
+                      (sys.exc_info()[1], dirname))
+        if not g_catch_exceptions:
+            raise
+    p = Player()
+    p.player_id = player_id
+    p.playername = dirname
+    z = p.playername.rfind('/')
+    if -1 != z:
+        p.playername = p.playername[z + 1:]
+    p.play = None
+    if None == m or not hasattr(m, 'play'):
+        logging.error('%s has no function "play"; ignoring ...' % dirname)
+    else:
+        p.play = getattr(m, 'play')
+    p.elapsed = 0.0
+    p.calls = 0
+    p.get_play = lambda x, y, z: call_player(p, (x, y, z), random.choice((1, 2, 3)))
+    return p
+
+
+def usage():
+    print('''\
+liars dice -- see http://github.com/botfights/liarsdice for dox
+
+usage:
+
+    $ python liarsdice.py <command> [<option> ...] [<arg> ...]
+
+commands:
+
+    game [<player1>] [<player2>] ...
+
+                        play a single game between players
+
+    tournament [<player1>] [<player2>] ...
+
+                        play a tournament between players
+options:
+
+    -h, --help                      show this help
+    --seed=<s>                      set seed for random number generator
+    --catch-exceptions=<off|on>     catch and log exceptions
+    --num-games=<n>                 set number of games for tournament (defaults to 100)
+
+    --log-level=<n>                 set log level (10 debug, 20 info, 40 error)
+''')
+
+
+def main(argv):
+    if 1 > len(argv):
+        usage()
+        sys.exit()
+    command = argv[0]
+    try:
+        opts, args = getopt.getopt(argv[1:], "h", [
+                                                    "help",
+                                                    "seed=",
+                                                    "catch-exceptions=",
+                                                    "num-games=",
+                                                    "log-level=",
+                                                    ])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(1)
+    seed = time.time()
+    num_games = 100
+    log_level = logging.DEBUG
+    global g_catch_exceptions
+    g_catch_exceptions = False
+    for o, a in opts:
+        if 0:
+            pass
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("--seed", ):
+            seed = a
+        elif o in ("--num-games", ):
+            num_games = int(a)
+        elif o in ("--log-level", ):
+            log_level = int(a)
+        elif o in ("--catch-exceptions", ):
+            g_catch_exceptions = 'off' != a
+        else:
+            raise Exception("unhandled option")
+    random.seed(seed)
+
+    if 0:
+        pass
+
+    elif 'game' == command:
+        logging.basicConfig(level=log_level, format='%(message)s', 
+                        stream=sys.stdout)
+        player1 = make_player('a', args[0])
+        player2 = make_player('b', args[1])
+        winner = play_game(race_to, player1, player2)
+        sys.exit()
+
+    elif 'tournament' == command:
+        logging.basicConfig(level=log_level, format='%(message)s', 
+                        stream=sys.stdout)
+        players = []
+        for player_id, playername in enumerate(args):
+            players.append(make_player(chr(ord('a') + player_id), playername))
+        play_tournament(num_games, race_to, players)
+        sys.exit()
+
+    else:
+        print 'i don\'t know how to "%s".' % command
+        usage()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
